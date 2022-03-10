663e74f859eed37c0ed6080e8516fb018829fd01
pathipatireshma <pathipati.reshma@gmail.com>
2022-03-10 19:18:53 +0530

    adding a echo

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -7,13 +7,14 @@ if [ -z "${INSTANCE_NAME}" ]; then
   exit
 fi
 
+AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
-  AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
-  if [-z "${AMI_ID}" ]; then
+if [-z "${AMI_ID}" ]; then
     echo -e "\e[1;31mUnable to find Image AMI_ID\e[0m"
-    else
+    exit
+else
     echo  -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
-  fi
+      fi
 
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
