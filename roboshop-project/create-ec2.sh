b005305eb043f5edef35c6938e9588472d3a501f
pathipatireshma <pathipati.reshma@gmail.com>
2022-03-10 22:06:53 +0530

    adding a echo

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -16,5 +16,11 @@ else
     echo  -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
       fi
 
+PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANCE_NAME}  --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
+if[ -z "${PRIVATE_IP}" ]; then
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+else
+  echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
+exit
+fi
