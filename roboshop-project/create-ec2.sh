ffb67dd2570f606026574541d4c183fcfff85504
pathipatireshma <pathipati.reshma@gmail.com>
2022-03-11 19:21:41 +0530

    adding a echo

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -25,7 +25,7 @@ PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTAN
 
 if [ -z "${PRIVATE_IP}" ]; then
     # Find Security Group
-    SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all-ports --query "SecurityGroups[*].GroupId" --output text)
+    SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values='allow all ports' --query "SecurityGroups[*].GroupId" --output text)
     if [ -z "${SG_ID}" ]; then
       echo -e "\e[1;33m Security Group 'allow all ports' does not exist"
       exit
