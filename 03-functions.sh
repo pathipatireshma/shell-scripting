dd44b9ec004730d12daa6a9920338743a9133529
pathipatireshma <pathipati.reshma@gmail.com>
2022-03-13 17:22:28 +0530

    adding a echo

--- 03-functions.sh
+++ 03-functions.sh
@@ -2,8 +2,28 @@
 
 ##Declare a function
 SAMPLE() {
-  echo "WELCOME TO DEVOPS TRAINING"
-}
+  a=20
+    b=20
+    echo "Welcome to DevOps training"
+    echo "Welcome to DevOps training"
+    return 1
+    echo "Welcome to DevOps training"
+    echo "Welcome to DevOps training"
+  }
 
-## Call the function
-SAMPLE
+  ## Main Program
+  ## Call the function
+  a=10
+  SAMPLE
+  SAMPLE
+  b=10
+
+  SAMPLE1() {
+    echo First Argument in Function = $1
+    return 1
+  }
+
+  echo First Argument in Main Program = $1
+  SAMPLE1 ABC
+  echo $?
+
