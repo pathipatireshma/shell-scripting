fd05d62cc63a45526bb396451033af75232db2cf
pathipatireshma <pathipati.reshma@gmail.com>
2022-03-14 10:48:15 +0530

    adding a echo

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -10,6 +10,6 @@ if [ -f components/$1.sh ]; then
   bash components/$1.sh
 else
   echo -e "\e[1;31mInvalid Input\e[0m"
-  echo -e "\e[1;33mAvailable Inputs - frontend|mongodb|catalogue|redis|user|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
+  echo -e "\e[1;33mAvailable Inputs - frontend|mongodb|catalogue|redis|users|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
   exit 1
 fi
